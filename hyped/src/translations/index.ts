409cbc209557db49dcc1077e06ac61ae4666b7ad
Saransh Rajput <saranshraj9101@gmail.com>
2026-02-13 14:31:50 +0530

    add translation

--- hyped/src/translations/index.ts
+++ hyped/src/translations/index.ts
@@ -1,9 +1,21 @@
 import { en } from './en';
 import { hi } from './hi';
+import { sa } from './sa';
+import { ta } from './ta';
+import { te } from './te';
+import { kn } from './kn';
+import { bn } from './bn';
+import { gu } from './gu';
 
 const languages = {
   en,
   hi,
+  sa,
+  ta,
+  te,
+  kn,
+  bn,
+  gu,
 } as const;
 
 export type LanguageCode = keyof typeof languages;
